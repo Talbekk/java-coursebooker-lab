4386f131de78f9509a0b802ee1d4dd49186f3486
Talbekk <mt.campbell@outlook.com>
2020-01-23 13:05:51 +0000

    adds test to find courses by rating

--- src/test/java/com/codeclan/example/coursebooker/CoursebookerApplicationTests.java
+++ src/test/java/com/codeclan/example/coursebooker/CoursebookerApplicationTests.java
@@ -1,13 +1,40 @@
 package com.codeclan.example.coursebooker;
 
+import com.codeclan.example.coursebooker.models.Course;
+import com.codeclan.example.coursebooker.repository.BookingRepository;
+import com.codeclan.example.coursebooker.repository.CourseRepository;
+import com.codeclan.example.coursebooker.repository.CustomerRepository;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @SpringBootTest
 class CoursebookerApplicationTests {
 
+	@Autowired
+	BookingRepository bookingRepository;
+
+	@Autowired
+	CourseRepository courseRepository;
+
+	@Autowired
+	CustomerRepository customerRepository;
+
 	@Test
 	void contextLoads() {
 	}
+	@Test
+	public void findCourseByRating(){
+		List<Course> found = courseRepository.findCourseByRating(4);
+		assertEquals("Dragon Riding", found.get(0).getName());
+	}
+
+
+
+
 
 }
